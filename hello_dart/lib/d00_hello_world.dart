db61fa0860fa73ef151ed440f48107bcf1dfcca6
wuwenying <wuwenying@kuaishou.com>
2021-05-28 20:19:39 +0800

    dart案例初识

---
+++ hello_dart/lib/d00_hello_world.dart
@@ -0,0 +1,9 @@
+/// 1. Dart语言的入口就是main函数，并且必须显式地进行定义;
+/// 2. 入口函数main没有返回值；
+/// 3. 传递给main的命令行参数，通过List<String>完成的；
+/// 4. 定义字符串可以使用单引号或者双引号
+/// 5. 必须使用分号结尾
+
+main(List<String> args) {
+  print('Hello World');
+}

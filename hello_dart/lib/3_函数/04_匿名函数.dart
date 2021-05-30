f586727950f7131e39b8ba23de983af488698f9b
wuwenying <wuwenying@kuaishou.com>
2021-05-30 21:17:01 +0800

    匿名函数

---
+++ hello_dart/lib/3_函数/04_匿名函数.dart
@@ -0,0 +1,16 @@
+main(List<String> args) {
+  // 1.定义数组
+  var movies = ['盗梦空间', '星际穿越', '少年派', '大话西游'];
+
+  // 2.使用forEach遍历: 有名字的函数
+  printElement(item) {
+    print(item);
+  }
+  movies.forEach(printElement);
+
+  // 3.使用forEach遍历: 匿名函数
+  movies.forEach((item) {
+    print(item);
+  });
+  movies.forEach((item) => print(item));
+}

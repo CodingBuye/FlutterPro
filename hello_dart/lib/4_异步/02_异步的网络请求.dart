b30d412ada83c1736a9ea1c44fea7b5c4c060314
wuwenying <wuwenying@kuaishou.com>
2021-06-07 18:10:56 +0800

    异步网络请求

--- hello_dart/lib/4_异步/02_异步的网络请求.dart
+++ hello_dart/lib/4_异步/02_异步的网络请求.dart
@@ -2,13 +2,23 @@ import 'dart:io';
 
 main(List<String> args) {
   print("main function start");
-  print(getNetworkData());
+  // 使用变量接收getNetworkData返回的future
+  var future = getNetworkData();
+  // 当future实例有返回结果时，会自动回调then中传入的函数
+  // 该函数会被放入到事件循环中，被执行
+  future.then((value) {
+    print(value);
+  }).catchError((error) { // 捕获出现异常时的情况
+    print(error);
+  });
+  print(future);
   print("main function end");
 }
 
 Future<String> getNetworkData() {
   return Future<String>(() {
     sleep(Duration(seconds: 3));
-    return 'network data';
+    // return 'network data';
+    throw Exception("网络请求出现错误");
   });
 }

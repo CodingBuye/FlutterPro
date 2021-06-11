35f93b33fa97b98f7b8443ea9612a2b8d6fa390f
wuwenying <wuwenying@kuaishou.com>
2021-06-11 18:42:53 +0800

    Form 表单验证

--- learn_flutter/lib/main.dart
+++ learn_flutter/lib/main.dart
@@ -37,7 +37,7 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text('coder'),
         ),
-        body: MyTextField(),
+        body: FormDemo(),
       ),
     );
   }
@@ -383,6 +383,73 @@ class _TextFieldDemoState extends State<TextFieldDemo> {
       onSubmitted: (value) {
         print('onsubmitted:$value');
       },
+    );
+  }
+}
+
+/// Form表单
+class FormDemo extends StatefulWidget {
+  @override
+  FormDemoState createState() {
+    return FormDemoState();
+  }
+}
+
+class FormDemoState extends State<FormDemo> {
+
+  final registerFormKey = GlobalKey<FormState>();
+  String? username, password;
+
+  void registerForm() {
+    registerFormKey.currentState!.save();
+    print('username: $username password:$password');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: registerFormKey,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          TextFormField(
+            autovalidateMode: AutovalidateMode.always,
+            decoration: InputDecoration(
+              icon: Icon(Icons.people),
+              labelText: '用户名或手机号'
+            ),
+            onSaved: (value) {
+              this.username = value;
+            },
+            validator: (value) {
+              if(value!.isEmpty) {
+                return '账号不能为空';
+              }
+              return null;
+            },
+          ),
+          TextFormField(
+            obscureText: true,
+            decoration: InputDecoration(
+              icon: Icon(Icons.lock),
+              labelText: '密码'
+            ),
+            onSaved: (value) {
+              this.password = value;
+            },
+          ),
+          SizedBox(height: 16,),
+          Container(
+            width: double.infinity,
+            height: 44,
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(primary: Colors.lightGreen),
+              child: Text('注 册', style: TextStyle(fontSize: 20, color: Colors.white),),
+              onPressed: registerForm,
+            ),
+          )
+        ],
+      ),
     );
   }
 }
